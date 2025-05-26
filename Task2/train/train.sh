7321cb29448654fe6596a3eea16ca47aff824858
Ruiqi Tang <RuiQiTarng@gmail.com>
2025-05-26 21:59:03 +0800

    Update:

--- Task2/train/train.sh
+++ Task2/train/train.sh
@@ -7,11 +7,11 @@ python \
     --out results_mask_rcnn.pkl \
     --show-dir vis_mask_rcnn
 
-# train for Sparse R-CNN
-mkdir Task2/output/sparse_rcnn_r50_fpn_1x_voc
-python  \
-    Task2/mmdetection/tools/train.py    \
-    Task2/mmdetection/configs/voc/sparse-rcnn_r50_fpn_1x_voc.py   \
-    Task2/output/sparse_rcnn_r50_fpn_1x_voc/latest.pth  \
-    --out results_sparse_rcnn.pkl   \
-    --show-dir vis_sparse_rcnn
+# # train for Sparse R-CNN
+# mkdir Task2/output/sparse_rcnn_r50_fpn_1x_voc
+# python  \
+#     Task2/mmdetection/tools/train.py    \
+#     Task2/mmdetection/configs/voc/sparse-rcnn_r50_fpn_1x_voc.py   \
+#     Task2/output/sparse_rcnn_r50_fpn_1x_voc/latest.pth  \
+#     --out results_sparse_rcnn.pkl   \
+#     --show-dir vis_sparse_rcnn
