9f2779e3ec0570b1b1bcdd7645be23eeba7b9ecb
RichTang <RuiQiTarng@gmail.com>
2025-05-29 23:34:16 +0800

    Delete Task2 directory

--- Task2/Task2Instructuion.md
+++
@@ -1,3 +0,0 @@
-# Preprocess
-
-1. Run ![Task2/preprocess/Download](Task2/)
--- Task2/preprocess/Config_for_RCNN.sh
+++
@@ -1,8 +0,0 @@
-# 拷贝配置文件（以 Mask R-CNN 为例）
-cd mmdetection
-pip install -r requirements/build.txt
-pip install -v -e .
-pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.0/index.html  # 替换为你实际使用的 CUDA 和 torch 版本
-
-cp configs/mask_rcnn/mask-rcnn_r50_fpn_1x_coco.py configs/voc/mask-rcnn_r50_fpn_1x_voc.py
-cp configs/sparse_rcnn/sparse-rcnn_r50_fpn_1x_coco.py configs/voc/sparse-rcnn_r50_fpn_1x_voc.py
